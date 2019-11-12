ad4968d0101f83461fc28289b541958a1ccb0608
demi <demi.mgga@gmail.com>
2019-11-12 21:01:32 +0200

    fix warning

--- src/scene.rs
+++ src/scene.rs
@@ -25,11 +25,11 @@ impl Sphere {
         let a = ray.direction().dot(&ray.direction());
         let b = 2.0 * ray.direction().dot(&oc);
         let c = oc.dot(&oc) - self.radius * self.radius;
-        let D = b * b - 4.0 * a * c;
-        if D < 0. {
+        let discriminant = b * b - 4.0 * a * c;
+        if discriminant < 0. {
             return -1.;
         } else {
-            return (-b - D.sqrt()) / (2.0 * a)
+            return (-b - discriminant.sqrt()) / (2.0 * a)
         }
     }
 
@@ -65,11 +65,11 @@ impl MovingSphere {
         let a = ray.direction().dot(&ray.direction());
         let b = 2.0 * ray.direction().dot(&oc);
         let c = oc.dot(&oc) - self.radius * self.radius;
-        let D = b * b - 4.0 * a * c;
-        if D < 0. {
+        let discriminant = b * b - 4.0 * a * c;
+        if discriminant < 0. {
             return -1.;
         } else {
-            return (-b - D.sqrt()) / (2.0 * a)
+            return (-b - discriminant.sqrt()) / (2.0 * a)
         }
     }
 
